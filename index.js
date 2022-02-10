af72333f305d18e9fd01c5ad3a3a9931f017f421
Jon Kaplon <jon.kaplon1@gmail.com>
2022-02-10 10:35:09 -0800

    commit tree a33fdce7ffb4a3aa1088773ce22c01465450c94b

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-function fetchBooks() {
+const url = "https://anapioficeandfire.com/api/books";
+
+function fetchBooks(render) {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(url)
+    .then((r) => r.json())
+    .then((render) => renderBooks(render));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
